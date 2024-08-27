a222d81f8376dc1cfefaf7562d3088d4b7e96afc
leprincemardoch <doublepo56@gmail.com>
2024-08-27 14:38:58 +0000

    MAJ - V3 - Structure & script maj

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
 class Config:
 
     class files:
-        articles_urls_list = 'articles_urls_list.txt'
+        save_folder = 'save_files'
+        articles_urls_list = f'{save_folder}/articles_urls_list.txt'
+        save_resume = f'{save_folder}/articles_resume.txt'
+        save_scrapping_failed = f'{save_folder}/not_scrapping.txt'
 
     class API_KEY:
-        groq_api_key = ""
+        groq_api_key = "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"
 
 
     class instruction_system:
         instruction = """ 
-SPEAK ONLY IN FRENCH
-You are used to summarize articles that will be given to you.
-Everything given to you must be summarized without comment. You make the summary without other unnecessary comments.
-Here are instructions to be strictly followed:
-- Give answers directly without introductory sentences
-- You must take what the article is about, just what the article is about and nothing else
-- Be as simple as possible
-- First, you state precisely what the article is about, then you make a summary, afterwards you summarize the article in a few points and you take the points
+            SPEAK ONLY IN FRENCH
+            You are used to summarize articles that will be given to you.
+            Everything given to you must be summarized without comment. You make the summary without other unnecessary comments.
+            Here are instructions to be strictly followed:
+            - Give answers directly without introductory sentences
+            - You must take what the article is about, just what the article is about and nothing else
+            - Be as simple as possible
+            - First, you state precisely what the article is about, then you make a summary, afterwards you summarize the article in a few points and you take the points
         """
--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from config import Config
 groq_api_key = Config.API_KEY.groq_api_key
 instruction = Config.instruction_system.instruction
 urls_lists_file = Config.files.articles_urls_list
+resume_file = Config.files.save_resume
+scrapping_failed_file = Config.files.save_scrapping_failed
 
 client = Groq(api_key=groq_api_key)
 
 
 def save_not_valid_url(url, error_cause):
     ''' Write in a file that the URL scraping failed '''
-    with open('not_scrapping.txt', 'a') as file:
+    with open(scrapping_failed_file, 'a') as file:
         file.write(f'{url} - Status code : {error_cause}\n{'-'*20}\n')
 
 def get_articles_urls_list():
@@ -38,11 +40,11 @@ def get_url_response(url):
         save_not_valid_url(url)
         return None
 
-def get_content(response: requests.Response):
-    if not response:
+def get_content(response):
+    if response is None:
         print(f'Response is {type(response)}')
         return None
-    
+
     status_code = response.status_code
 
     if status_code != 200:
@@ -52,6 +54,8 @@ def get_content(response: requests.Response):
     return response.content
     
 def get_text_article(content):
+    if content is None:
+        return None
     html = BeautifulSoup(content, 'html.parser')
     article = html.find('body').text
 
@@ -61,14 +65,14 @@ def get_text_article(content):
 
 class Groq_ai:
     @staticmethod
-    def resume(articles):
-        if articles is None:
+    def resume(instruction, article):
+        if article is None:
             return None
         completion = client.chat.completions.create(
             model="llama-3.1-70b-versatile",
             messages=[
                 {"role": "system", "content": instruction},
-                {"role": "user", "content": articles}
+                {"role": "user", "content": article}
             ],
             temperature=1,
             max_tokens=2000,
@@ -83,18 +87,21 @@ def save_resume(resume, url):
     if resume is None:
         return None
 
-    with open('articles_resume.txt', 'a') as w:
+    with open(resume_file, 'a') as w:
         w.write(f'url = {url}{'\n'*2}{'-'*10}\n{resume} {'\n'*2} {'-'*50}{'\n'*2}')
         
     print(f'Url = {url} : SUCCESS WRITE')
     return 'Success wirte'
 
-if __name__ == "__min__":
+if __name__ == "__main__":
     articles_urls = get_articles_urls_list()
     for url in articles_urls:
         requests_response = get_url_response(url)
+        if requests_response is None:
+            pass
+
         requests_content = get_content(requests_response)
         text_article = get_text_article(requests_content)
-        resume_article = Groq_ai.resume(text_article)
-        save_resume(resume=resume_article)
+        resume_article = Groq_ai.resume(instruction, text_article)
+        save_resume(url=url, resume=resume_article)
     print('\n\n FINISH')
